a5e64a69fae6e468491018f1edd552f48bb22eed
sebasCM8 <sebastiancalderon21800@gmail.com>
2023-06-04 21:09:43 -0400

    dumb error fixed

--- src/database/firestore.js
+++ src/database/firestore.js
@@ -3,8 +3,8 @@ const { initializeApp, applicationDefault, cert } = require('firebase-admin/app'
 const { getFirestore, Timestamp, FieldValue } = require('firebase-admin/firestore');
 
 //FIREBASE AUTH KEYS
-const serviceAccount = require("../../ServiceAccountFirebase.json");
-// const serviceAccount = require("../../sebas2023-7eb58-firebase-adminsdk-rcna8-0e0dd0713b.json");
+//const serviceAccount = require("../../ServiceAccountFirebase.json");
+const serviceAccount = require("../../sebas2023-7eb58-firebase-adminsdk-rcna8-0e0dd0713b.json");
 //FIREBASE INITIALIZATION
 initializeApp({
     credential: cert(serviceAccount)
